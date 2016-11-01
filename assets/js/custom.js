074beac7322300a37136e4d1244538cd61a1550b
huong <huongnt057@gmail.com>
2016-11-01 08:51:17 +0700

    tree and index

---
+++ assets/js/custom.js
@@ -0,0 +1,18 @@
+$(function () {
+	
+    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+    $('.tree li.parent_li > span').on('click', function (e) {
+        var children = $(this).parent('li.parent_li').find(' > ul > li');
+        if (children.is(":visible")) {
+            children.hide('fast');
+            $(this).addClass('activetree'),
+            $(this).attr('title', 'Expand this branch').find(' > i.fa-minus').addClass('fa-plus').removeClass('fa-minus');
+            
+        } else {
+            children.show('fast');
+            $(this).attr('title', 'Collapse this branch').find(' > i.fa-plus').addClass('fa-minus').removeClass('fa-plus');
+        }
+        e.stopPropagation();
+    });
+   
+});
